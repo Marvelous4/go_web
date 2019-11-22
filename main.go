f172d0df9224c226367dd3671ea5fd8e2dc57e01
marvelous4 <mavgbenro@gmail.com>
2019-11-22 22:37:55 +0100

    add logging to terminal

--- main.go
+++ main.go
@@ -1,22 +1,48 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+        "fmt"
+        "log"
+        "net/http"
 )
 
 func main() {
-	http.HandleFunc("/welcome", welcome)
+        mux := http.NewServeMux()
 
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/", fs)
+        v1Mux := http.NewServeMux()
 
-	if err := http.ListenAndServe(":3001", nil); err != nil {
-		log.Fatal("ListenAndServer ::", err)
-	}
+        v1Mux.HandleFunc("/profile", func(w http.ResponseWriter, r *http.Request) {
+                fmt.Fprintln(w, "v1 Profile")
+        })
+
+        v1Mux.HandleFunc("/posts", func(w http.ResponseWriter, r *http.Request) {
+                fmt.Fprintln(w, "v1 Posts")
+        })
+
+        v2Mux := http.NewServeMux()
+
+        v2Mux.HandleFunc("/profile", func(w http.ResponseWriter, r *http.Request) {
+                fmt.Fprintln(w, "v2 Profile")
+        })
+
+        v2Mux.HandleFunc("/posts", func(w http.ResponseWriter, r *http.Request) {
+                fmt.Fprintln(w, "v2 Posts")
+        })
+
+        mux.Handle("/v1/", http.StripPrefix("/v1", v1Mux))
+        mux.Handle("/v2/", http.StripPrefix("/v2", v2Mux))
+
+        loggedHandler := loggingMiddleware(mux)
+
+        if err := http.ListenAndServe(":3001", loggedHandler); err != nil {
+                fmt.Println(err)
+        }
 }
 
-func welcome(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Welcome to the Go webserver codelab")
+func loggingMiddleware(handler http.Handler) http.Handler {
+        return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+                log.Printf("Got a %s request for: %v\n", r.Method, r.URL)
+                handler.ServeHTTP(w, r)
+                log.Printf("Handler finished processing request")
+        })
 }
